5761f3a6dde880817ceac3c183128f5e81e6516d
vincentCav <vin100.cav@gmail.com>
2023-09-19 16:30:06 -0500

    S3 copying function, errors, readMe tutorials

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ For Windows:
 
 <h2>Contributing to the Edmunds scraper</h2>
 
-For scraping additional info (most/all of the project todos are scraping more info)
+<h3>For scraping additional info (most/all of the project todos are scraping more info)</h3>
 
 1. Make sure the project runs after you've forked the repo. If not, contact me.
 2. Downloading Postman to understand and search the website is strongly recommended. Otherwise, use inspect on your browser
@@ -37,7 +37,28 @@ For scraping additional info (most/all of the project todos are scraping more in
 5. After solving (check with print statements) use an "assert" statement to protect against changes in the Edmunds websites.
 6. Commit and push to your own forked repo, then enter a pull request. This can be a little touch and go, so contact me if you run into issues.
 
+<h3>For accessing AWS and configuring appropriate files:</h3>
+
+1. You might not need to. I'll be working on some basic developer tools like a postman mockup so that you don't need AWS credentials to make the program work in a way that makes sense. I'll update here for more as I learn more.
+2. If you have to contribute to AWS, contact me and I'll set up a single sign on MFA account so you can add to Redshift and S3 aspects of the project.
+3. See the tutorials and guides section below for more on what has been helpful to learn Redshift, S3, and cloud subjects.
+
 <h2>Tutorials and Guides</h2>
 Credit to realpython.com's tutorial <a href="https://realpython.com/beautiful-soup-web-scraper-python/">Beautiful Soup: Build a Web Scraper With Python</a> for the introduction to webscraping.
 Credit to the AWS tutorial <a href="https://docs.aws.amazon.com/redshift/latest/dg/tutorial-loading-data.html">Loading Data from Amazon S3</a> for the introduction to using the redshift database.
-Credit to the AWS tutorial <a href="https://docs.aws.amazon.com/redshift/latest/mgmt/python-connect-examples.html">
+Credit to the AWS tutorial <a href="https://docs.aws.amazon.com/redshift/latest/mgmt/python-connect-examples.html">redshift python connect</a> for docs on the python-redshift interaction
+
+Credit to <a href="https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html#using-boto3"> the Boto3 quickstart</a> for introducing me to boto3, essential to the copying csv to s3 aspect of this project.
+Credit to <a href="https://stackoverflow.com/a/65950645"> this stackoverflow answer</a> for explaining why the above quickstart needs to be configured differently for sso sessions and how to do that.
+
+Credit to the <a href="https://docs.aws.amazon.com/singlesignon/latest/userguide/getting-started.html">AWS IAM Identity Center user guide</a> for how to set up IAM Identity Center and its users.
+Credit to the <a href="https://docs.aws.amazon.com/cli/latest/userguide/sso-configure-profile-token.html">AWS CLI configure with a Single Sign on Session</a> for how to programmatically make use of the above IAM Identity Center to make credentials.
+
+<h2>Open Source Datasets Used</h2>
+This includes either sources currently in use, or sources that I really want to use to enhance the quality of the end project.
+<a href="https://www.kaggle.com/datasets/CooperUnion/cardataset">Car Features and MSRP</a> by CopperUnion
+<a href="https://www.kaggle.com/datasets/akshaydattatraykhare/car-details-dataset">Car Details Dataset</a> by AKSHAY DATTATRAY KHARE, from 'Car Dekho'.
+<a href="https://www.kaggle.com/code/melikedilekci/eda-car-data-analysis">EDA Car Data Analysis</a> by MELIKE DILEKCI, from 'Car Dekho'.
+<a href="https://www.kaggle.com/datasets/austinreese/craigslist-carstrucks-data">Vehicle Listings from Craigslist.org</a>, a great resource from Austin Reese, also see <a href="https://github.com/AustinReese/UsedVehicleSearch">AustinReese's github</a>
+<a href="https://www.kaggle.com/datasets/rkiattisak/car-ownership-predictionbeginner-intermediate">Ownership of Cars dataset</a> from Kiattisak Rattanaporn
+<a href="https://www.kaggle.com/datasets/suraj520/car-sales-data">Car Sales database</a> by SURAJ. I don't know the source, but it's data on salespersons and commissions for a year by the car.
--- one-off-scrapers/edmunds_scraper.py
+++ one-off-scrapers/edmunds_scraper.py
@@ -6,6 +6,23 @@ import requests
 import bs4
 import csv
 
+config = {"make": "toyota", "model": "highlander"}
+
+payload = {}
+headers = {
+    "Cookie": 'EdmundsYear="&zip=75019&dma=623:IP&city=Coppell&state=TX&lat=32.96451&lon=-96.984901"; ak_bmsc=E8A6B9608D960ED91DFF93C11D8987F7~000000000000000000000000000000~YAAQKjsvF8aZAm6KAQAASTDxbxXOyg4+PjwJN9BDhpc2J/QZLRfqTw5iA+ztHcXy35t4erI5sBq2nOloWZuF350Kja7AVlmy3bE7MkwsqHQRt0uRB3pIntjUPl0etQmTxJIaoQdGGOTvNj2cL/oXloKUQAE0JbQjgftpweb75Zx7uj9yrVNtf/Tb7X0vxQYQrWk+RJbwMUMl50vp3MFY42xOkRRvBG6MTo746P1+PkliWc2jPK/b+Pna7gZwqUgw3aTt3iyLft9FICr48YRoZG2vQpazoTzm7nlJB1LMcrfHI0YTaVPSupSzdTn5dC2ypp6besC3I58tocXSNPTRzcb9QLMCPPu3EGk+3AOEY2hyTmseQrlcbmhy2+cGHQ==; edmunds=da97a56c-761c-4483-b86e-f3c9b0ec6135; edw=471883652886893950; entry_page=new_used_car_inventory_srp; entry_url=www.edmunds.com%2Finventory%2Fsrp.html; entry_url_params=%7B%7D; location=j%3A%7B%22zipCode%22%3A%2275019%22%2C%22type%22%3A%22Standard%22%2C%22areaCode%22%3A%22940%22%2C%22timeZone%22%3A%22Central%22%2C%22gmtOffset%22%3A-6%2C%22dst%22%3A%221%22%2C%22latitude%22%3A32.96451%2C%22longitude%22%3A-96.984901%2C%22salesTax%22%3A0.0625%2C%22dma%22%3A%22623%22%2C%22dmaRank%22%3A5%2C%22stateCode%22%3A%22TX%22%2C%22city%22%3A%22Coppell%22%2C%22county%22%3A%22Dallas%22%2C%22inPilotDMA%22%3Atrue%2C%22state%22%3A%22Texas%22%2C%22ipDma%22%3A%22623%22%2C%22ipStateCode%22%3A%22TX%22%2C%22ipZipCode%22%3A%2275019%22%2C%22userIP%22%3A%2247.187.127.217%22%2C%22userSet%22%3Anull%7D; session-id=471883652886893950; usprivacy=1NNN; visitor-id=da97a56c-761c-4483-b86e-f3c9b0ec6135; content-targeting=US,TX,IRVING,623,-96.9488,32.8139,75014-75017+75038-75039+75060-75063; device-characterization=false,false; feature-flags=j%3A%7B%22speedcurve%22%3Afalse%2C%22sentry%22%3Afalse%7D',
+    "Cache-Control": "no-cache",
+    "User-Agent": "PostmanRuntime/7.32.3",
+    "Accept": "*/*",
+    "Connection": "keep-alive",
+}
+
+base_url = "https://www.edmunds.com"
+url = base_url + "/inventory/srp.html?inventorytype=used%2Ccpo&make={0}&model={0}%7C{1}".format(
+    config["make"], config["model"]
+)
+
+totalCars = 0
 
 def extractYearMakeModelTrimTuple(src):
     """Takes the text src and returns a 4-tuple."""
@@ -14,7 +31,10 @@ def extractYearMakeModelTrimTuple(src):
     year = src_split[0]
     make = src_split[1]
     model = src_split[2]
-    trim = functools.reduce((lambda x, y: str(x) + " " + str(y)), src_split[3:])
+    if src_split[3:]:
+        trim = functools.reduce((lambda x, y: str(x) + " " + str(y)), src_split[3:])
+    else:
+        trim = ""
     return (year, make, model, trim)
 
 
@@ -36,68 +56,86 @@ def cleanMiles(strM):
 def cleanOffer(offerTag):
     """Takes a tag object and returns"""
     offer_text = offerTag["aria-label"]
-    print(offer_text)
     assert (offer_text[0] == "$"), "var 'offer_text' must start with dollar sign. offer_text = {}".format(offer_text)
     # There is an offer visible
     val = offerTag["aria-label"].split(" ")[0]
     return int(re.sub(r"([$,])", "", val))
 
-config = {"make": "toyota", "model": "highlander"}
+def getNextPage(soup):
+    nextPageSuffix = soup.find(class_="pagination-btn rounded d-flex align-items-center justify-content-center text-blue-30 mx-1_5").next_sibling.next_sibling.get("href")
+    assert nextPageSuffix, 'End of pages (nextPageSuffix = null)'
+    return base_url + nextPageSuffix
 
-url = "https://www.edmunds.com/inventory/srp.html?inventorytype=used%2Ccpo&make={0}&model={0}%7C{1}".format(
-    config["make"], config["model"]
-)
+def scrapeForCarMakeAndModel(make, model):
+    url = base_url + "/inventory/srp.html?inventorytype=used%2Ccpo&make={0}&model={0}%7C{1}".format(
+        make, model
+    )
 
-payload = {}
-headers = {
-    "Cookie": 'EdmundsYear="&zip=75019&dma=623:IP&city=Coppell&state=TX&lat=32.96451&lon=-96.984901"; ak_bmsc=E8A6B9608D960ED91DFF93C11D8987F7~000000000000000000000000000000~YAAQKjsvF8aZAm6KAQAASTDxbxXOyg4+PjwJN9BDhpc2J/QZLRfqTw5iA+ztHcXy35t4erI5sBq2nOloWZuF350Kja7AVlmy3bE7MkwsqHQRt0uRB3pIntjUPl0etQmTxJIaoQdGGOTvNj2cL/oXloKUQAE0JbQjgftpweb75Zx7uj9yrVNtf/Tb7X0vxQYQrWk+RJbwMUMl50vp3MFY42xOkRRvBG6MTo746P1+PkliWc2jPK/b+Pna7gZwqUgw3aTt3iyLft9FICr48YRoZG2vQpazoTzm7nlJB1LMcrfHI0YTaVPSupSzdTn5dC2ypp6besC3I58tocXSNPTRzcb9QLMCPPu3EGk+3AOEY2hyTmseQrlcbmhy2+cGHQ==; edmunds=da97a56c-761c-4483-b86e-f3c9b0ec6135; edw=471883652886893950; entry_page=new_used_car_inventory_srp; entry_url=www.edmunds.com%2Finventory%2Fsrp.html; entry_url_params=%7B%7D; location=j%3A%7B%22zipCode%22%3A%2275019%22%2C%22type%22%3A%22Standard%22%2C%22areaCode%22%3A%22940%22%2C%22timeZone%22%3A%22Central%22%2C%22gmtOffset%22%3A-6%2C%22dst%22%3A%221%22%2C%22latitude%22%3A32.96451%2C%22longitude%22%3A-96.984901%2C%22salesTax%22%3A0.0625%2C%22dma%22%3A%22623%22%2C%22dmaRank%22%3A5%2C%22stateCode%22%3A%22TX%22%2C%22city%22%3A%22Coppell%22%2C%22county%22%3A%22Dallas%22%2C%22inPilotDMA%22%3Atrue%2C%22state%22%3A%22Texas%22%2C%22ipDma%22%3A%22623%22%2C%22ipStateCode%22%3A%22TX%22%2C%22ipZipCode%22%3A%2275019%22%2C%22userIP%22%3A%2247.187.127.217%22%2C%22userSet%22%3Anull%7D; session-id=471883652886893950; usprivacy=1NNN; visitor-id=da97a56c-761c-4483-b86e-f3c9b0ec6135; content-targeting=US,TX,IRVING,623,-96.9488,32.8139,75014-75017+75038-75039+75060-75063; device-characterization=false,false; feature-flags=j%3A%7B%22speedcurve%22%3Afalse%2C%22sentry%22%3Afalse%7D',
-    "Cache-Control": "no-cache",
-    "User-Agent": "PostmanRuntime/7.32.3",
-    "Accept": "*/*",
-    "Connection": "keep-alive",
-}
+    csv_file = open('car_entries.csv', mode="w")
+    fieldnames = ["car_entry_id","year","make","model","trim","miles","offer"]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
 
-response = requests.request("GET", url, headers=headers, data=payload, timeout=5)
+    getCarsOnPage(url, writer)
 
-soup = bs4.BeautifulSoup(response.content, "html.parser")
-results = soup.find_all(class_="d-flex mb-0_75 mb-md-1_5 col-12 col-md-6")
+def getCarsOnPage(url, writer):
+    global totalCars
+    try:
+        response = requests.request("GET", url, headers=headers, data=payload, timeout=1)
+    except requests.exceptions.ReadTimeout:
+        print("Read timed out with URL = '{}'\nretrying with timeout=5.".format(url))
+        response = requests.request("GET", url, headers=headers, data=payload, timeout=5)
 
-car_data = dict()
+    soup = bs4.BeautifulSoup(response.content, "html.parser")
+    results = soup.find_all(class_="d-flex mb-0_75 mb-md-1_5 col-12 col-md-6")
+    for idx, car_result in enumerate(results):
+        try:
+            getCarOnPage(car_result, totalCars, writer)
+        except RuntimeError as err:
+            print("RuntimeError: Could not parse car_result for car_id = {0} due to {1}".format(totalCars, err))
+        
+        totalCars += 1
+    
+    try:
+        nextPage = getNextPage(soup)
+    except AssertionError:
+        print("End of pages")
+        return
+    
+    getCarsOnPage(nextPage, writer)
 
-csv_file = open('car_entries.csv', mode="w")
-fieldnames = ["car_entry_id","year","make","model","trim","miles","offer"]
-writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-writer.writeheader()
-
-for idx, car_result in enumerate(results):
+def getCarOnPage(rawCarSoup, car_id, writer):
     # get Year, Make, Model and Trim
-    yearMakeModelAndTrim = car_result.find("a", class_="usurp-inventory-card-vdp-link")[
+    car_data = dict()
+    yearMakeModelAndTrim = rawCarSoup.find("a", class_="usurp-inventory-card-vdp-link")[
         "aria-label"
     ]
-    car_id_key = create_car_id_key(yearMakeModelAndTrim, idx)
+    # print(yearMakeModelAndTrim)
+    car_id_key = create_car_id_key(yearMakeModelAndTrim, car_id)
     year, make, model, trim = extractYearMakeModelTrimTuple(yearMakeModelAndTrim)
     car_data.update({car_id_key: dict()})
     car_data[car_id_key].update(
-        {"year": year, "make": make, "model": model, "trim": trim, "car_entry_id": idx}
+        {"year": year, "make": make, "model": model, "trim": trim, "car_entry_id": car_id}
     )
 
     # get Miles traveled
-    miles = get_neighbor_relative_to_title_tag(car_result.find(title="Car Mileage"))
-    cleaned_miles = cleanMiles(miles)
-    car_data[car_id_key].update({"miles": cleaned_miles})
+    try:
+        miles = get_neighbor_relative_to_title_tag(rawCarSoup.find(title="Car Mileage"))
+        cleaned_miles = cleanMiles(miles)
+        car_data[car_id_key].update({"miles": cleaned_miles})
+    except ValueError:
+        car_data[car_id_key].update({"miles": None})
+        raise RuntimeError("Failed to extract miles from {}".format(miles)) from ValueError
 
-    vehicleInfoClass = car_result.find(class_="vehicle-info d-flex flex-column p-1")
-
-    # get Offer traveled
-    offerTag = vehicleInfoClass.find(class_="usurp-inventory-card-vdp-link")
-    offerDollarAmount = cleanOffer(offerTag)
-    car_data[car_id_key].update({"offer": offerDollarAmount})
-
-    print(
-        "year: {}, trim: {}, miles: {}, offer: {}".format(
-            year, trim, cleaned_miles, offerDollarAmount
-        )
-    )
+    # get Offer
+    vehicleInfoClass = rawCarSoup.find(class_="vehicle-info d-flex flex-column p-1")
+    try:
+        offerTag = vehicleInfoClass.find(class_="usurp-inventory-card-vdp-link")
+        offerDollarAmount = cleanOffer(offerTag)
+        car_data[car_id_key].update({"offer": offerDollarAmount})
+    except AssertionError:
+        car_data[car_id_key].update({"offer": None})
+        raise RuntimeError("Failed to extract offer from {}".format(offerTag)) from AssertionError
 
     # TODO: Get # of drivers
 
@@ -105,13 +143,8 @@ for idx, car_result in enumerate(results):
 
     # TODO: Get Location (state)
 
-    # TODO: Write row to CSV file
     writer.writerow(car_data[car_id_key])
 
-# TODO: Copy to S3
+# # TODO: Copy to S3
 
-
-# print(car_data)
-# print(results)
-
-# print(response.text)
+scrapeForCarMakeAndModel("honda", "civic")
